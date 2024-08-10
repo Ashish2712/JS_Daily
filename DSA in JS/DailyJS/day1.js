afe2195a929d56b667aeef283ba6f06ae2f136a8
Ashish <akushwah206@gmail.com>
2024-08-10 23:36:32 +0530

    day1

---
+++ DSA in JS/DailyJS/day1.js
@@ -0,0 +1,8 @@
+const name = "Ashish"
+const date = "10th August"
+// console.log(name + " todays date is " + date)
+
+console.log([] == 1) // false
+console.log([] === 1) //false
+console.log([] == 0)   // true
+console.log([] === 0)  // false
