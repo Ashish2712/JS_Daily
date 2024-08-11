f2c0f25e7a2ad6e844be6563e9fa96ef5b98e660
Ashish <akushwah206@gmail.com>
2024-08-11 22:28:18 +0530

    day1 and two learning

--- DSA in JS/DailyJS/day1.js
+++ DSA in JS/DailyJS/day1.js
@@ -1,8 +1,42 @@
-const name = "Ashish"
-const date = "10th August"
+// const name = "Ashish"
+// const date = "10th August"
 // console.log(name + " todays date is " + date)
 
-console.log([] == 1) // false
-console.log([] === 1) //false
-console.log([] == 0)   // true
-console.log([] === 0)  // false
+// console.log([] == 1) // false
+// console.log([] === 1) //false
+// console.log([] == 0)   // true
+// console.log([] === 0)  // false
+
+// const ritikRuns = {name: "Ritik", total: 0, balls: {type: "leather"}}
+// const otherRuns = {...ritikRuns}
+// otherRuns.balls.type="Tennis"
+
+// console.log(ritikRuns)
+// console.log(otherRuns)
+
+// output
+// { name: 'Ritik', total: 0, balls: { type: 'Tennis' } }
+// { name: 'Ritik', total: 0, balls: { type: 'Tennis' } }
+
+
+
+// const ritikRuns = {name: "Ritik", total: 0}
+// const otherRuns = {...ritikRuns}
+// otherRuns.total = 90
+
+// console.log(ritikRuns)
+// console.log(otherRuns)
+
+//output 
+// { name: 'Ritik', total: 0 }
+// { name: 'Ritik', total: 90 }
+
+// const ritikRuns = {name: "Ritik", total: 0}
+// const otherRuns = ritikRuns
+// otherRuns.total = 90
+
+// console.log(ritikRuns)
+// console.log(otherRuns)
+//output
+// { name: 'Ritik', total: 90 }
+// { name: 'Ritik', total: 90 }
